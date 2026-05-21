4a147c0d55185a206c4b27ebb94c22796c741f18
sree90274-maker <sree90274@gmail.com>
2026-05-21 22:33:29 +0530

    tasks

---
+++ tasks.py
@@ -0,0 +1,14 @@
+text = "Python is very easy"
+print(text[::-1])
+
+
+print("...........")
+
+#Build a binary permission checker using bitwise operators.
+print(6&3)
+print(6|3)
+print(6^3)
+print(~3)
+print(6<<3)
+print(6>>3)
+
